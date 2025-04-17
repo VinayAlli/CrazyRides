5c69141dc47bd2a97e1ab0a6466b6ec1749692d7
Vinay Viswanadh Alli <allivinay2001@gmail.com>
2025-04-17 13:44:01 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_npk8pzpI6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: ' 6hg8bgolage5tbgdpsvhpdhjbh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6hg8bgolage5tbgdpsvhpdhjbh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
